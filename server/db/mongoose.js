f64fae05562976c9148a820546dd2c1478e29feb
NK987 <phonebat987@gmail.com>
2019-02-03 18:12:56 +0530

    add modified files

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODBURL||'mongodb://localhost:27017/ToDoApp',{ useNewUrlParser: true });
 
 
 module.exports={mongoose};
