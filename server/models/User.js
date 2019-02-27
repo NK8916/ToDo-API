5e2dfbfb191b759c1a0b5dd8edb9a33d0f552442
NK987 <phonebat987@gmail.com>
2019-02-27 06:56:55 +0530

    add modified files

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 const validator=require('validator');
 const jwt=require('jsonwebtoken');
 const _=require('lodash');
-
+const bcrypt=require('bcryptjs');
 
 var UserSchema=new mongoose.Schema({
     email:{
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON=function(){
     var user=this;
     var userObject=user.toObject();
 
-    return _.pick(userObject,['email','password']);
+    return _.pick(userObject,['_id','email']);
 };
 
 UserSchema.methods.generateAuthToken=function(){
@@ -70,6 +70,44 @@ UserSchema.statics.findByToken=function(token){
         'tokens.access':'auth'
     });
 };
+
+UserSchema.statics.findByCredentials=function(email,password){
+    var User=this;
+
+    return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        }
+
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,function(err,res){
+                if(!res){
+                    reject();
+                }
+
+                    resolve(user);
+            })
+        })
+    });
+};
+
+
+UserSchema.pre('save',function(next){
+    var user=this;
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+               user.password=hash;
+               next();
+            })
+        });
+    }
+
+    else{
+        next();
+    }
+});
 
 var User=mongoose.model('User',UserSchema);
 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 const {ObjectID}=require('mongodb');
+const bcrypt=require('bcryptjs');
 var {mongoose}=require('./db/mongoose');
 var {ToDo}=require('./models/ToDo');
 var {User}=require('./models/User');
 var _=require('lodash');
 var {authenticate}=require('./middleware/authenticate');
+
 
 var app=express();
 app.use(bodyParser.json());
@@ -113,6 +115,36 @@ app.post('/users',(req,res)=>{
 
 app.get('/users/me',authenticate,(req,res)=>{
     res.send(req.user);
+})
+
+// app.post('/users/login',(req,res)=>{
+//     var body=_.pick(req.body,['email','password']);
+//     console.log(body);
+//     User.find({email:body.email}).then((user)=>{
+//         console.log(user[0].password);
+//         bcrypt.compare(body.password,user[0].password,(err,resStatus)=>{
+//             if(!resStatus){
+//                return res.status(401).send();
+//             }
+
+//             res.send({body});
+//         })
+//     }).catch((e)=>{
+//         res.status(401).send(e);
+//     })
+// })
+
+
+app.post('/users/login',(req,res)=>{
+    var body=_.pick(req.body,['email','password']);
+
+    User.findByCredentials(body.email,body.password).then((user)=>{
+        return user.generateAuthToken().then((token)=>{
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 })
 
 app.listen(port,()=>{
